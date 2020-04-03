8e22459a5c4b470c308bf297fa088425502fbbad
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-04-04 00:27:46 +0530

    records fetched

--- db/crud.sql
+++ db/crud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 02, 2020 at 12:20 AM
+-- Generation Time: Apr 04, 2020 at 12:25 AM
 -- Server version: 5.7.29-0ubuntu0.18.04.1
 -- PHP Version: 7.2.24-0ubuntu0.18.04.3
 
@@ -39,7 +39,15 @@ CREATE TABLE `records` (
 --
 
 INSERT INTO `records` (`id`, `first_name`, `last_name`, `created_at`, `updated_at`) VALUES
-(16, 'Rakesh', 'Rankawat', '2020-04-01 18:45:01', '2020-04-01 18:45:01');
+(16, 'Rakesh', 'Rankawat', '2020-04-01 18:45:01', '2020-04-01 18:45:01'),
+(17, 'Meenu', 'Rana', '2020-04-03 18:47:36', '2020-04-03 18:47:36'),
+(18, 'Dela', 'Smith', '2020-04-03 18:47:51', '2020-04-03 18:47:51'),
+(19, 'Effin', 'Cray', '2020-04-03 18:48:05', '2020-04-03 18:48:05'),
+(20, 'Erika', 'Albright', '2020-04-03 18:51:47', '2020-04-03 18:51:47'),
+(21, 'Mark', 'Zeck', '2020-04-03 18:52:37', '2020-04-03 18:52:37'),
+(22, 'Edvardo', 'Sheverine', '2020-04-03 18:54:10', '2020-04-03 18:54:10'),
+(23, 'Tanuj', 'Chauhan', '2020-04-03 18:54:31', '2020-04-03 18:54:31'),
+(24, 'Umesh', 'Kumar', '2020-04-03 18:54:45', '2020-04-03 18:54:45');
 
 --
 -- Indexes for dumped tables
@@ -59,7 +67,7 @@ ALTER TABLE `records`
 -- AUTO_INCREMENT for table `records`
 --
 ALTER TABLE `records`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.php
+++ index.php
@@ -1,4 +1,17 @@
-<?php include('layout/header.php'); ?>
+<?php require('layout/header.php'); ?>
+
+<?php require('db.php'); ?>
+
+<?php
+
+$sql = 'SELECT * FROM records ORDER BY created_at DESC';
+
+$records = [];
+if($connect->query($sql)) {
+  $records = $connect->query($sql);
+} 
+
+?>
 
 <div class="row my-4">
   <div class="col-md-10">
@@ -20,16 +33,18 @@
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>John</td>
-      <td>Doe</td>
-      <td>March 31, 2020</td>
-      <td>
-        <a href="#" class="btn btn-warning">Update</a>
-        <a href="#" class="btn btn-danger">Delete</a>
-      </td>
-    </tr>
+    <?php foreach($records as $index => $record): ?>
+      <tr>
+        <th scope="row"><?php echo $index+1; ?></th>
+        <td><?php echo $record['first_name']; ?></td>
+        <td><?php echo $record['last_name']; ?></td>
+        <td><?php echo $record['updated_at']; ?></td>
+        <td>
+          <a href="#" class="btn btn-warning">Update</a>
+          <a href="#" class="btn btn-danger">Delete</a>
+        </td>
+      </tr>
+    <?php endforeach; ?>
   </tbody>
 </table>
   
